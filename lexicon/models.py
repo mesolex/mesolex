39fa1610b887451ebb0d65f32d55043e862421ed
nmashton <neil.m.ashton@gmail.com>
2018-05-08 15:15:49 -0400

    Adding basic LexicalEntry model
    For simplicity of importing data and getting something up to look at and
    iterate on, we're going to start by abusing Postgres and treating it as
    a quasi document database, directly importing the XML data and stashing
    it in the `data` attribute in JSON-converted form.

---
+++ lexicon/models.py
@@ -0,0 +1,14 @@
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class LexicalEntry(models.Model):
+    ref = models.CharField(_("Unique ID"), max_length=64)
+    data = JSONField()
+
+    def __str__(self):
+        if 'lx' in self.data:
+            return self.data['lx']
+        else:
+            return self.ref or 'Word #%s' % (self.id)
