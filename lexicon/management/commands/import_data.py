43786066cf4452f319780026c7a4a5fd96b225c1
nmashton <neil.m.ashton@gmail.com>
2018-05-09 07:13:55 -0400

    Updating XML import to use Parker convention (our data doesn't have attrs!) and have better reporting

--- lexicon/management/commands/import_data.py
+++ lexicon/management/commands/import_data.py
@@ -1,6 +1,5 @@
-import json
 import xml.etree.ElementTree as ET
-from xmljson import badgerfish as bf
+from xmljson import parker as xml_parser
 
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
@@ -22,27 +21,39 @@ class Command(BaseCommand):
 
         updated_entries = 0
         added_entries = 0
+        errors = 0
+        error_messages = []
 
         for lx_group in root.getchildren():
-            lx_group_dict = bf.data(lx_group)
-            data = lx_group_dict['lxGroup']
-            if isinstance(data['ref'], list):
-                ref = data['ref'][0]
-            else:
-                ref = data['ref']['$']
-            (lexical_entry, created, ) = (models.LexicalEntry.objects.update_or_create(
-                ref=ref,
-                headword=data['lx']['$'],
-                defaults={
-                    'data': data,
-                }
-            ))
+            (lexical_entry, created, ) = (None, None, )
+            try:
+                data = xml_parser.data(lx_group)
+                if isinstance(data['ref'], list):
+                    ref = data['ref'][0]
+                else:
+                    ref = data['ref']
+                (lexical_entry, created, ) = (models.LexicalEntry.objects.update_or_create(
+                    ref=ref,
+                    headword=data['lx'],
+                    defaults={
+                        'data': data,
+                    }
+                ))
 
-            if created:
-                added_entries += 1
-            else:
-                updated_entries += 1
+                if created:
+                    self.stdout.write('.', ending='')
+                    added_entries += 1
+                else:
+                    self.stdout.write('u', ending='')
+                    updated_entries += 1
+            except Exception as e:
+                self.stdout.write('E', ending='')
+                errors += 1
+                error_messages.append(str(e))
 
-        self.stdout.write('Added %s entries, updated %s' % (
-            added_entries, updated_entries,
+        if error_messages:
+            self.stderr.write('\n\n'.join(error_messages))
+
+        self.stdout.write('\n\nTOTAL: %s added, %s updated, %s errors' % (
+            added_entries, updated_entries, errors,
         ))
