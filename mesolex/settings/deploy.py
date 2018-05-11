d8c8a5c6c3d2fd3ba473bde2dc3962f0d5258da8
nmashton <neil.m.ashton@gmail.com>
2018-05-11 17:42:43 -0400

    Add "database" Ansible setup
    Rename server to "staging"

---
+++ mesolex/settings/deploy.py
@@ -0,0 +1,13 @@
+from .base import *
+
+ENVIRONMENT = os.environ['ENVIRONMENT']
+
+DEBUG = False
+
+DATABASES['default']['NAME'] = os.environ.get('DB_NAME', 'mesolex_%s' % ENVIRONMENT.lower())
+DATABASES['default']['USER'] = os.environ.get('DB_USER', 'mesolex_%s' % ENVIRONMENT.lower())
+DATABASES['default']['PASSWORD'] = os.environ.get('DB_PASSWORD', '')
+
+ALLOWED_HOSTS = [os.environ['DOMAIN']] + [
+    dn for dn in os.environ.get('ADDITIONAL_DOMAINS', '').split(',') if dn
+]
