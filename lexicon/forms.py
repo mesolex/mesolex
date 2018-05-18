bf1755c7cc62e092e2ef77fa9b4cae83fe78bf55
nmashton <neil.m.ashton@gmail.com>
2018-05-18 06:34:03 -0400

    Starting to construct dynamic filters
    Adds a formset that will be used to construct search filters. By
    grabbing `{{ formset.empty_form }}`, we can let the user dynamically add
    and remove copies of the form that can be used to construct the overall
    filter.

--- lexicon/forms.py
+++ lexicon/forms.py
@@ -3,5 +3,28 @@ from django import forms
 from haystack.forms import ModelSearchForm
 
 
+BOOLEAN_OPERATORS = (
+    ('&&', 'AND'),
+    ('||', 'OR'),
+)
+
+FILTERABLE_FIELDS = (
+    ('headword', 'Headword'),
+)
+
+
 class LexiconSearchForm(ModelSearchForm):
     q = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mr-3'}))
+
+
+class LexicalSearchFilterForm(forms.Form):
+    query_string = forms.CharField()
+    operator = forms.ChoiceField(
+        choices=BOOLEAN_OPERATORS,
+    )
+    filter_on = forms.ChoiceField(
+        choices=FILTERABLE_FIELDS,
+    )
+
+
+LexicalSearchFilterFormset = forms.formset_factory(LexicalSearchFilterForm)
--- lexicon/views.py
+++ lexicon/views.py
@@ -1,7 +1,15 @@
 from haystack.generic_views import SearchView
 
-from .forms import LexiconSearchForm
+from .forms import (
+    LexiconSearchForm,
+    LexicalSearchFilterFormset
+)
 
 
 class LexiconSearchView(SearchView):
     form_class = LexiconSearchForm
+
+    def get_context_data(self, **kwargs):
+        if 'formset' not in kwargs:
+            kwargs['formset'] = LexicalSearchFilterFormset()
+        return super().get_context_data(**kwargs)
