c63e3f392f0a4c174560dccddbd109b45ab52fa6
nmashton <neil.m.ashton@gmail.com>
2018-07-11 07:05:49 -0400

    Adding messages files & form in header to switch between languages

--- mesolex/settings/base.py
+++ mesolex/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,6 +150,11 @@ HAYSTACK_CONNECTIONS = {
 }
 
 LANGUAGE_CODE = 'es'
+
+LANGUAGES = [
+    ('es', _('español')),
+    ('en', _('inglés')),
+]
 
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
