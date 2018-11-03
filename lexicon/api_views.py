3dd9b29b080a4e8a2e0aa965eec46844566397bc
nmashton <neil.m.ashton@gmail.com>
2018-11-03 10:49:52 -0400

    Django 2.1 upgrade

--- lexicon/api_views.py
+++ lexicon/api_views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 
 from . import models, serializers
 
 
 class LexicalEntryViewSet(viewsets.ModelViewSet):
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
     serializer_class = serializers.LexicalEntrySerializer
     queryset = models.LexicalEntry.objects.all()
