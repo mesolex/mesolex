12aa61097a0880a2e9f97639801e91033665c69e
nmashton <neil.m.ashton@gmail.com>
2018-12-10 13:51:58 -0500

    Foist most query-building form functionality onto pluggable app

--- lexicon/forms.py
+++ lexicon/forms.py
@@ -6,33 +6,15 @@ from django.conf import settings
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
-from .utils import (
+from query_builder.forms import (
+    QueryBuilderForm,
+    QueryBuilderBaseFormset,
+)
+
+from mesolex.utils import (
     to_vln,
-    ForceProxyEncoder,
 )
 
-BOOLEAN_OPERATORS = (
-    ('and', 'y'),
-    ('or', 'o'),
-    ('and_n', 'y no'),
-    ('or_n', 'o no'),
-)
-
-FILTERS = (
-    ('begins_with', _('Empieza con')),
-    ('ends_with', _('Termina con')),
-    ('contains', _('Incluye')),
-    ('exactly_equals', _('Es exactamente igual a')),
-    ('regex', _('Expresión regular')),
-)
-
-FILTERS_DICT = {
-    'begins_with': '__istartswith',
-    'ends_with': '__iendswith',
-    'contains': '__icontains',
-    'exactly_equals': '',
-    'regex': '__iregex',
-}
 
 # TODO: investigate why these gettext-strings have to be
 # lazy to work as expected when serialized by the formset.
@@ -55,22 +37,10 @@ FILTERABLE_FIELDS_DICT = {
 }
 
 
-class LexicalSearchFilterForm(forms.Form):
-    query_string = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    operator = forms.ChoiceField(
-        choices=BOOLEAN_OPERATORS,
-        widget=forms.Select(attrs={'class': 'custom-select'})
-    )
-    filter = forms.ChoiceField(
-        choices=FILTERS,
-        widget=forms.Select(attrs={'class': 'custom-select'})
-    )
-    filter_on = forms.ChoiceField(
-        choices=FILTERABLE_FIELDS,
-        widget=forms.Select(attrs={'class': 'custom-select'})
-    )
+class LexicalSearchFilterForm(QueryBuilderForm):
+    FILTERABLE_FIELDS = FILTERABLE_FIELDS
+    FILTERABLE_FIELDS_DICT = FILTERABLE_FIELDS_DICT
+
     vln = forms.BooleanField(required=False)
 
     def get_filter_action_and_query(self):
@@ -91,86 +61,20 @@ class LexicalSearchFilterForm(forms.Form):
         if form_data['vln']:
             (filter_arg_val, query_string,) = to_vln(filter_str, form_data['query_string'])
         else:
-            filter_arg_val = FILTERS_DICT.get(filter_str, '')
+            filter_arg_val = self.FILTERS_DICT.get(filter_str, '')
             query_string = form_data['query_string']
         
         return (filter_arg_val, query_string)
 
-    def get_query(self):
-        if not self.is_bound:
-            return
-        
-        (filter_arg_val, query_string) = self.get_filter_action_and_query()
-        filter_on_str = self.cleaned_data['filter_on']
-        filter_on_vals = FILTERABLE_FIELDS_DICT.get(filter_on_str, [])
-
-        query_expression = Q()
-
-        for filter_on_val in filter_on_vals:
-            query_expression |= Q(**{'%s%s' % (filter_on_val, filter_arg_val): query_string})
-
-        return query_expression
-
-
-    def clean(self):
-        cleaned_data = super().clean()
-        if 'filter' in cleaned_data and cleaned_data['filter'] == 'regex':
-            qs = cleaned_data['query_string']
-            try:
-                re.compile(qs)
-            except Exception:
-                self.add_error('query_string', forms.ValidationError(_('Expresión regular no válida.')))
-
 
 # TODO: create an abstract superclass that this is an extension of.
-class BaseLexiconQueryComposerFormset(forms.BaseFormSet):
+class BaseLexiconQueryComposerFormset(QueryBuilderBaseFormset):
     FILTERABLE_FIELDS = FILTERABLE_FIELDS
 
     CONTROLLED_VOCAB_FIELDS = {
         'part_of_speech': settings.LANGUAGE_CONFIGURATION['azz']['part_of_speech'],
         'inflectional_type': settings.LANGUAGE_CONFIGURATION['azz']['inflectional_type'],
     }
-
-    @property
-    def configuration_data(self):
-        config = {
-            'filterable_fields': self.FILTERABLE_FIELDS,
-            'controlled_vocab_fields': self.CONTROLLED_VOCAB_FIELDS,
-        }
-        return config
-    
-    @property
-    def configuration_data_as_json(self):
-        return json.dumps(
-            self.configuration_data,
-            ensure_ascii=False,
-            cls=ForceProxyEncoder,
-        )
-    
-    def get_full_query(self):
-        query = None
-
-        for form in self.forms:
-            if form.is_valid():
-                form_q = form.get_query()
-                operator = form.cleaned_data['operator']
-                if not query:
-                    if operator == 'and_n' or operator == 'or_n':
-                        query = ~form_q
-                    else:
-                        query = form_q
-                else:
-                    if operator == 'and':
-                        query &= form_q
-                    elif operator == 'or':
-                        query |= form_q
-                    elif operator == 'and_n':
-                        query &= (~form_q)
-                    elif operator == 'or_n':
-                        query |= (~form_q)
-        
-        return query
-
 
 
 LexicalSearchFilterFormset = forms.formset_factory(
---
+++ query_builder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QueryBuilderConfig(AppConfig):
+    name = 'query_builder'
