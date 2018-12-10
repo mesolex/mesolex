8684bad95cef84369795fa3092f4bc529435e16b
nmashton <neil.m.ashton@gmail.com>
2018-12-10 14:11:11 -0500

    Extract <Vln/> component to prepare for generalized search front-end

--- lexicon/static/jsx/search/search-formset.jsx
+++ lexicon/static/jsx/search/search-formset.jsx
@@ -8,6 +8,44 @@ import { controlledVocabCheck } from '../util';
 import SearchForm from './search-form';
 
 
+const Vln = ({
+  i,
+  config,
+  dataset,
+  onChangeFieldFrom,
+}) => {
+  const isControlled = controlledVocabCheck(config.controlled_vocab_fields || {});
+
+  return (
+    isControlled(dataset.filter_on)
+      ? null
+      : (
+        <div className="form-check mt-2">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id={`id_form-${i}-vln`}
+            name={`form-${i}-vln`}
+            checked={dataset.vln}
+            disabled={dataset.filter === 'regex'}
+            onChange={onChangeFieldFrom('vln', 'checked')}
+          />
+          <label htmlFor={`id_form-${i}-vln`} className="form-check-label">
+            {gettext('Neutralizar cantidad vocálica')}
+          </label>
+        </div>
+      )
+  );
+};
+
+Vln.propTypes = {
+  i: PropTypes.number.isRequired,
+  config: PropTypes.shape.isRequired,
+  dataset: PropTypes.shape.isRequired,
+  onChangeFieldFrom: PropTypes.func.isRequired,
+};
+
+
 export default class SearchFormSet extends React.Component {
   static propTypes = {
     formsetConfig: PropTypes.shape.isRequired,
@@ -177,6 +215,14 @@ export default class SearchFormSet extends React.Component {
               errors={this.state.formsetIndexedErrors[uniqueId]}
               onChangeFieldFrom={this.onChangeFieldFrom(uniqueId)}
               removeFilter={this.removeFilter(uniqueId)}
+              extraFilterComponents={[
+                <Vln
+                  i={i}
+                  config={this.props.formsetConfig}
+                  dataset={this.state.formsetIndexedDatasets[uniqueId]}
+                  onChangeFieldFrom={this.onChangeFieldFrom(uniqueId)}
+                />,
+              ]}
             />
           ))
         }
