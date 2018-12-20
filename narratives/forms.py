d8d778345c089d223ab99d35fc954f8cf6231da3
nmashton <neil.m.ashton@gmail.com>
2018-12-20 12:54:20 -0500

    Controlled vocab fields for narratives

--- narratives/forms.py
+++ narratives/forms.py
@@ -1,16 +1,20 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 
 from query_builder.forms import (
     QueryBuilderForm,
     QueryBuilderBaseFormset,
 )
 
+from narratives import models
+
 
 FILTERABLE_FIELDS = (
-    ('subgenre', 'subgenre'),
-    ('consultant_name', 'consultant name'),
-    ('village_of_recording', 'village of recording'),
-    ('title', 'title'),
+    ('subgenre', _('Subgénero')),
+    ('consultant_name', _('Nombre del contribuidor')),
+    ('village_of_recording', _('Pueblo de grabacion')),
+    ('title', _('Título')),
 )
 FILTERABLE_FIELDS_DICT = {
     'subgenre': ('subgenre', ),
@@ -27,6 +31,24 @@ class SoundMetadataSearchFilterForm(QueryBuilderForm):
 
 class BaseSoundMetadataQueryComposerFormset(QueryBuilderBaseFormset):
     FILTERABLE_FIELDS = FILTERABLE_FIELDS
+
+    @property
+    def controlled_vocab_fields(self):
+        subgenres = set([x['subgenre'] for x in models.SoundMetadata.objects.values('subgenre')])
+        con1_names = set([x['contr1'] for x in models.SoundMetadata.objects.values('contr1')])
+        con2_names = set([x['contr2'] for x in models.SoundMetadata.objects.values('contr2')])
+        consultant_names = con1_names.union(con2_names)
+
+        con1_qs = models.SoundMetadata.objects.filter(con1_role='Consultant')
+        con2_qs = models.SoundMetadata.objects.filter(con2_role='Consultant')
+        con1_vs = set([x['con1_origin'] for x in con1_qs.values('con1_origin')])
+        con2_vs = set([x['con2_origin'] for x in con2_qs.values('con2_origin')])
+        villages = con1_vs.union(con2_vs)
+        return {
+            'consultant_name': [(x, x) for x in sorted(consultant_names) if x],
+            'subgenre': [(x, x) for x in sorted(subgenres) if x],
+            'village_of_recording': [(x, x) for x in sorted(villages) if x],
+        }
 
 SoundMetadataQueryComposerFormset = forms.formset_factory(
     SoundMetadataSearchFilterForm,
