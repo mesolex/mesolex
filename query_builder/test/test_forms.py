913b6f791fab8523be757ca445190b9571da72c8
nmashton <neil.m.ashton@gmail.com>
2019-02-10 18:56:28 -0500

    Add tests

--- query_builder/test/test_forms.py
+++ query_builder/test/test_forms.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock
 
+import django.forms
+from django.db.models import Q
 from django.test import TestCase
 
 from query_builder import forms
@@ -46,4 +48,101 @@ class QueryBuilderFormTestCase(TestCase):
         self.assertEqual(
             ('contains', '__something_else', 'baz', data),
             fake_transformation_two.call_args[0],
+        )
+    
+
+class QueryBuilderBaseFormsetTestCase(TestCase):
+    def test_works_without_global_filters(self):
+        class TestForm(forms.QueryBuilderForm):
+            FILTERABLE_FIELDS = [
+                ('bar', 'Bar',),
+            ]
+            FILTERABLE_FIELDS_DICT = {
+                'bar': ('bar', ),
+            }
+
+        test_formset = django.forms.formset_factory(
+            TestForm,
+            formset=forms.QueryBuilderBaseFormset,
+        )
+
+        formset_data = {
+            "form-0-query_string": "foo",
+            "form-0-operator": "and",
+            "form-0-filter": "begins_with",
+            "form-0-filter_on": "bar",
+            "form-INITIAL_FORMS": "0",
+            "form-MAX_NUM_FORMS": "1000",
+            "form-MIN_NUM_FORMS": "0",
+            "form-TOTAL_FORMS": "1",
+        }
+
+        bound_formset = test_formset(formset_data)
+
+        self.assertTrue(bound_formset.is_valid())
+        
+        query = bound_formset.get_full_query()
+
+        self.assertEqual(
+            [('bar__istartswith', 'foo')],
+            query.children,
+        )
+
+    def test_applies_global_filters(self):
+        class TestForm(forms.QueryBuilderForm):
+            FILTERABLE_FIELDS = [
+                ('bar', 'Bar',),
+            ]
+            FILTERABLE_FIELDS_DICT = {
+                'bar': ('bar', ),
+            }
+
+        class TestGlobalFilter(forms.QueryBuilderGlobalFiltersForm):
+            foo_bar = django.forms.BooleanField(required=False)
+            baz_qux = django.forms.BooleanField(required=False)
+            
+            def clean_foo_bar(self):
+                foo_bar = self.cleaned_data['foo_bar']
+                return Q(baz__isnull=(not foo_bar))
+
+            def clean_baz_qux(self):
+                baz_qux = self.cleaned_data['baz_qux']
+                return Q(qux__isnull=(baz_qux))
+
+        class TestFormset(forms.QueryBuilderBaseFormset):
+            global_filters_class = TestGlobalFilter
+        
+        test_formset = django.forms.formset_factory(
+            TestForm,
+            formset=TestFormset,
+        )
+
+        formset_data = {
+            "form-0-query_string": "foo",
+            "form-0-operator": "and",
+            "form-0-filter": "begins_with",
+            "form-0-filter_on": "bar",
+            "form-INITIAL_FORMS": "0",
+            "form-MAX_NUM_FORMS": "1000",
+            "form-MIN_NUM_FORMS": "0",
+            "form-TOTAL_FORMS": "1",
+            "foo_bar": "on",
+            "baz_qux": "on",
+        }
+
+        bound_formset = test_formset(formset_data)
+
+        self.assertTrue(bound_formset.is_valid())
+        
+        query = bound_formset.get_full_query()
+
+        self.assertEqual(3, len(query.children))
+        self.assertEqual('AND', query.connector)
+        self.assertIn(
+            ('baz__isnull', False),
+            query.children,
+        )
+        self.assertIn(
+            ('qux__isnull', True),
+            query.children,
         )
