945bca8aba917db4773657e5e3f2579df776adf6
Neil Ashton <neil.m.ashton@gmail.com>
2019-07-09 17:41:03 -0400

    Webpack and Eslint config & deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   module: {
@@ -7,74 +7,74 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.(css|scss)$/,
         loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
-            "postcss-loader",
+            'postcss-loader',
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      }
-    ]
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+    ],
   },
   entry: {
     home: [
-      "babel-polyfill",
-      path.join(__dirname, "mesolex/static/js/index.js")
+      'babel-polyfill',
+      path.join(__dirname, 'mesolex/static/js/index.js'),
     ],
     search: [
-      "babel-polyfill",
-      path.join(__dirname, "lexicon/static/jsx/search/index.jsx")
+      'babel-polyfill',
+      path.join(__dirname, 'lexicon/static/jsx/search/index.jsx'),
     ],
     narratives: [
-      "babel-polyfill",
-      path.join(__dirname, "narratives/static/jsx/index.jsx")
+      'babel-polyfill',
+      path.join(__dirname, 'narratives/static/jsx/index.jsx'),
     ],
-    lexicon_scss: path.join(__dirname, "lexicon/static/scss/index.scss")
+    lexicon_scss: path.join(__dirname, 'lexicon/static/scss/index.scss'),
   },
   output: {
-    path: path.join(__dirname, "mesolex/static/dist"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, 'mesolex/static/dist'),
+    filename: '[name].bundle.js',
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendors: {
-          name: "vendors",
-          chunks: "initial",
+          name: 'vendors',
+          chunks: 'initial',
           minChunks: 2,
-          minSize: 0
-        }
-      }
-    }
+          minSize: 0,
+        },
+      },
+    },
   },
   resolve: {
-    extensions: [".js", ".jsx", ".scss"],
+    extensions: ['.js', '.jsx', '.scss'],
     alias: {
-      lexicon: path.resolve(__dirname, "lexicon/static/jsx/search/"),
-      narratives: path.resolve(__dirname, "narratives/static/jsx/"),
-      "query-builder": path.resolve(__dirname, "query_builder/static/jsx/"),
-      "plyr-src": path.resolve(__dirname, 'node_modules/plyr/src/')
-    }
+      lexicon: path.resolve(__dirname, 'lexicon/static/jsx/search/'),
+      narratives: path.resolve(__dirname, 'narratives/static/jsx/'),
+      'query-builder': path.resolve(__dirname, 'query_builder/static/jsx/'),
+      'plyr-src': path.resolve(__dirname, 'node_modules/plyr/src/'),
+    },
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: "[name].bundle.css"
-    })
-  ]
+      filename: '[name].bundle.css',
+    }),
+  ],
 };
