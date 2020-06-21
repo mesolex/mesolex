28018fabd207856c90f76c97db44df9d2887f612
Neil Ashton <neil.m.ashton@gmail.com>
2020-06-21 12:38:46 -0400

    Add Wagtail 2.9

--- mesolex/urls.py
+++ mesolex/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, re_path, path
 from django.views.i18n import JavaScriptCatalog
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 import lexicon.urls
 import lexicon.views
@@ -25,14 +30,20 @@ import mesolex.views
 
 urlpatterns = [
     path('', mesolex.views.home, name='home'),
-    path('robots\.txt', include('robots.urls')),
+    path('robots.txt', include('robots.urls')),
     path('api/', include('lexicon.api_urls'), name='api'),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('narratives/', include(narratives.urls)),
     path('search/', include(lexicon.urls)),
+
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
 ]
+
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
 
 if settings.DEBUG:
     import debug_toolbar
