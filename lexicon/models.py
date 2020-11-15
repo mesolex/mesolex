19029e1f640be0f1a03fba47dbe208dcfe4c290b
Neil Ashton <neil.m.ashton@gmail.com>
2020-11-15 10:47:34 -0500

    Add models for simplified schema

--- lexicon/models.py
+++ lexicon/models.py
@@ -3,6 +3,58 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 
+class Entry(models.Model):
+    identifier = models.CharField(
+        max_length=256,
+        db_index=True,
+    )
+    parent_entry = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    value = models.CharField(
+        max_length=256,
+        db_index=True,
+    )
+    language = models.CharField(max_length=64)
+    other_data = JSONField(
+        blank=True,
+        null=True,
+    )
+
+
+class Searchable(models.Model):
+    class Meta:
+        abstract = True
+
+    entry = models.ForeignKey(
+        Entry,
+        on_delete=models.CASCADE,
+    )
+    language = models.CharField(max_length=64)
+    type_tag = models.CharField(
+        max_length=256,
+        db_index=True,
+    )
+    other_data = JSONField(
+        blank=True,
+        null=True,
+    )
+
+
+class SearchableString(Searchable):
+    value = models.CharField(
+        max_length=256,
+        db_index=True,
+    )
+
+
+class LongSearchableString(Searchable):
+    value = models.TextField()
+
+
 class ValidEntryManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().exclude(
