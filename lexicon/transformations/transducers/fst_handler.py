d9e91d5173fa2c04f3bbdb48f0329a4ba16434cf
Robert Pugh <robert.pugh@coursehero.com>
2021-04-11 07:45:14 -0700

    previously fst_handler was empty, also made minorr update to lexicons

---
+++ lexicon/transformations/transducers/fst_handler.py
@@ -0,0 +1,56 @@
+import attapply
+from retrie.trie import Trie
+from typing import List
+
+
+def get_nahuat_att_file(flex=True, isoglosses=False, vln=False):
+    att_fn = 'nahuat/att/in2dic'
+    if flex is True:
+        att_fn += '_flex'
+    if vln is True:
+        att_fn += '_vln'
+    if isoglosses is True:
+        att_fn += '_iso'
+
+    att_fn += '.att'
+
+    return att_fn
+
+
+class FSTHandler(object):
+    def __init__(self, path_to_att):
+        self.path_to_att = path_to_att
+        self.fst = attapply.ATTFST(self.path_to_att)
+
+    def generate_forms(self, w: str) -> List[str]:
+        """
+        Apply the transducer to an input word and get all generated forms.
+
+        :param w: The word input/searched by a user.
+        :return: List of forms generated from applying the FST to w.
+        """
+        return [w[0] for w in self.fst.apply(w)]
+
+    def match(self, w1: str, w2: str) -> bool:
+        """
+        Apply fst to w1 and check if any of its outputs match w2.
+
+        :param w1: The word input/searched by a user.
+        :param w2: A word from the dictionary
+        :return: True if one of the transduced forms of w1 is the same as w2.
+        """
+        return w2 in self.generate_forms(w1)
+
+    def get_pattern(self, w: str) -> str:
+        """
+        Get an efficient regex pattern matching all generated forms of a
+        given input word.
+
+        :param w: The word input/searched by a user.
+        :return: A string that can be compiled into a regex.
+        """
+        forms = self.generate_forms(w)
+        trie = Trie()
+        for w in forms:
+            trie.add(w)
+        return trie.pattern()
