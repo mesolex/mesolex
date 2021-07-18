2a606e6899c72a27638d3b0fa0880da33180fde9
Neil Ashton <neil.m.ashton@gmail.com>
2021-07-18 09:38:19 -0400

    Hook in additions

--- lexicon/forms/__init__.py
+++ lexicon/forms/__init__.py
@@ -2,6 +2,7 @@ from .azz import AzzLexicalSearchFilterFormset as AzzFormset
 from .juxt1235_verb import Juxt1235VerbLexicalSearchFilterFormset as Juxt1235VerbFormset
 from .juxt1235_non_verb import Juxt1235NonVerbLexicalSearchFilterFormset as Juxt1235NonVerbFormset
 from .trq import TrqLexicalSearchFilterFormset as TrqFormset
+from .plantnames_oax import PnoLexicalSearchFilterFormset as PnoFormset
 
 DEFAULT_FORMSET = AzzFormset
 
@@ -15,5 +16,7 @@ def formset_for_dataset(dataset_code):
         return Juxt1235VerbFormset
     if dataset_code == 'juxt1235_non_verb':
         return Juxt1235NonVerbFormset
+    if dataset_code == 'plantnames_oax':
+        return PnoFormset
 
     return DEFAULT_FORMSET
